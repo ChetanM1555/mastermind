9669efc2aae4b40146ee223a61f7d34f4acd86eb
ChetanM1555 <126090346+ChetanM1555@users.noreply.github.com>
2023-03-04 20:50:07 +0200

    Fixed input errors

--- first_java_project1/Main.java
+++ first_java_project1/Main.java
@@ -2,7 +2,7 @@
 // import java.lang.Math;
 import java.util.Random;
 import java.util.ArrayList;
-import java.util.Arrays;
+// import java.util.Arrays;
 // import java.util.List;
 import java.util.Scanner;
 
@@ -11,35 +11,52 @@ public class Main {
         boolean correct = false;
         ArrayList<Integer> code = random_code();
         System.out.println(code);
-        ArrayList<Integer> answer = get_input();
-        System.out.println(answer);
-        correct = compare(answer, code);
+        int count = 8;
+        do {
+            count--;
+            ArrayList<Integer> answer = get_input();
+            // System.out.println(answer);
+            try {
+                correct = compare(answer, code, count);
+            } catch (Exception e) {
+                System.out.println("Enter only 4 digits!");
+            }
+        } while (correct == false);
     }
 
     public static ArrayList<Integer> random_code() {
         Random rand = new Random();
         int rand_int1 = 10;
-        // int[] mastermind;
+
         ArrayList<Integer> mastermind = new ArrayList<>();
         for (int i = 0; i < 4; i++) {
-            rand_int1 = rand.nextInt(8);
+            rand_int1 = rand.nextInt(8) + 1;
             // NB remove duplicates
-            mastermind.add(rand_int1);
+            if (mastermind.contains(rand_int1)) {
+                i--;
+            } else {
+                mastermind.add(rand_int1);
+            }
         }
-        // System.out.println(mastermind);
+
         return mastermind;
     }
 
     public static ArrayList<Integer> get_input() {
-        // ArrayList<Integer> input_code = new ArrayList<>();
+        // NB only 4 digits
         int answer = 9;
         Scanner in = new Scanner(System.in);
 
-        while (!(answer < 9999 & answer > 1000)) {
-            System.out.print("Enter a 4 digit code: ");
-            answer = in.nextInt();
-        }
-        in.close();
+        do {
+            try {
+                System.out.print("Enter a 4 digit code: ");
+                answer = in.nextInt();
+
+            } catch (Exception e) {
+                // System.out.print("Enter a 4 digit code: ");
+                answer = 9;
+            }
+        } while (answer < 999 && answer > 9999);
 
         // convert input to an array
         ArrayList<Integer> answer_list = new ArrayList<Integer>();
@@ -51,13 +68,40 @@ public class Main {
         return answer_list;
     }
 
-    public static boolean compare(ArrayList<Integer> answer, ArrayList<Integer> code) {
-        if (Arrays.equals(answer, code) == true) {
-            System.out.println("Congratulations you guessed the code");
+    public static boolean compare(ArrayList<Integer> answer, ArrayList<Integer> code, int count) {
+        /*
+         * checks if answer is correct
+         */
+        if (answer.equals(code) == true) {
+            System.out.println("Congratulations you guessed the code!!");
+            return true;
+        } else if (count == 0) {
+            System.out.println("Out of turns.");
             return true;
         } else {
+            check_count_correct(answer, code);
             System.out.println("Try again");
+            System.out.println("Turns left: " + count);
             return false;
         }
+    }
+
+    public static void check_count_correct(ArrayList<Integer> answer, ArrayList<Integer> code) {
+        int correct_num = 0;
+        int correct_pos = 0;
+        // System.out.println(answer);
+        // System.out.println(code);
+        for (int i = 0; i < 4; i++) {
+            int a = answer.get(i);
+            int b = code.get(i);
+            if (a == b) {
+                correct_pos += 1;
+            } else if (code.contains(a)) {
+                correct_num += 1;
+            }
+        }
+        System.out.println("Numbers guessed correctly but not in the correct position are " + correct_num);
+        System.out.println("Numbers guessed in correct posittion are " + correct_pos);
+
     }
 }
